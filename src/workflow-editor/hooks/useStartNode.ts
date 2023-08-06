9424c804dd98f8d31f59bf17be0fc6947419236d
Water.Li <lizongbin@hotmail.com>
2023-08-06 10:53:27 +0800

    Update useStartNode.ts

--- src/workflow-editor/hooks/useStartNode.ts
+++ src/workflow-editor/hooks/useStartNode.ts
@@ -2,22 +2,26 @@ import { useCallback, useEffect, useState } from "react"
 import { IWorkFlowNode } from "../interfaces"
 import { useEditorEngine } from "./useEditorEngine"
 
+//获取起始节点
 export function useStartNode() {
   const [startNode, setStartNode] = useState<IWorkFlowNode>()
-  const store = useEditorEngine()
+  const engine = useEditorEngine()
 
+  //引擎起始节点变化事件处理函数
   const handleStartNodeChange = useCallback((startNode: IWorkFlowNode) => {
     setStartNode(startNode)
   }, [])
 
   useEffect(() => {
-    const unsub = store?.subscribeStartNodeChange(handleStartNodeChange)
+    //订阅起始节点变化事件
+    const unsub = engine?.subscribeStartNodeChange(handleStartNodeChange)
     return unsub
-  }, [handleStartNodeChange, store])
+  }, [handleStartNodeChange, engine])
 
+  //初始化时，先拿到最新数据
   useEffect(() => {
-    setStartNode(store?.store.getState().startNode)
-  }, [store?.store])
+    setStartNode(engine?.store.getState().startNode)
+  }, [engine?.store])
 
   return startNode
 }
