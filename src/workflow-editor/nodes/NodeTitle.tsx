73c53d47f01dc97b8c2805e9299c075f7f010399
Water.Li <lizongbin@hotmail.com>
2023-08-16 12:55:31 +0800

    Update NodeTitle.tsx

--- src/workflow-editor/nodes/NodeTitle.tsx
+++ src/workflow-editor/nodes/NodeTitle.tsx
@@ -55,6 +55,7 @@ export const Input = styled.input`
   outline: solid 2px rgba(80,80,80, 0.3);
   border: 0;
   border-radius: 4px;
+  color: ${props => props.theme?.token?.colorText};
 `
 
 export const NodeTitle = memo((props: {
